cf04c2f2231bc293b99c78b39d4ca423e7fad209
JJNETAIR <gbhd4u@gmail.com>
2026-05-25 23:53:23 +0300

    Delete OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++
@@ -1,6 +0,0 @@
-/**
- * Apple Air - OneSignal Service Worker
- * This file is required by OneSignal Web Push SDK
- * Place at root of your website
- */
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
